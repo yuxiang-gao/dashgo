4099535f8a3d2fadf175d27db14b7ee0fcf72262
DavidGogh <davidgogh96@gmail.com>
2017-06-10 16:43:19 +0800

    routine commit

--- dashgo_nav/nodes/nav_test.py
+++ dashgo_nav/nodes/nav_test.py
@@ -80,25 +80,49 @@ class NavTest():
         #     Point(-0.861, -0.019, 0.000),
         #     Quaternion(0.000, 0.000, 0.892, -0.451))
 
+        # locations['loc_1'] = Pose(
+        #     Point(-13.079, 10.644, 0.000),
+        #     Quaternion(0.000, 0.000, 0.952, 0.305))
+        # locations['loc_2'] = Pose(
+        #     Point(-8.872, 14.910, 0.000),
+        #     Quaternion(0.000, 0.000, -0.836, 0.549))
+        # locations['loc_3'] = Pose(
+        #     Point(-4.766, 12.545, 0.000),
+        #     Quaternion(0.000, 0.000, 0.950, 0.314))
+        # locations['loc_4'] = Pose(
+        #     Point(-1.727, 10.284, 0.000),
+        #     Quaternion(0.000, 0.000, 0.986, 0.169))
+        # locations['loc_5'] = Pose(
+        #     Point(0.947, 7.799, 0.000),
+        #     Quaternion(0.000, 0.000, 0.940, 0.340))
+        # locations['loc_6'] = Pose(
+        #     Point(-1.116, 5.282, 0.000),
+        #     Quaternion(0.000, 0.000, 0.450, 0.893))
         locations['loc_1'] = Pose(
-            Point(-13.079, 10.644, 0.000),
-            Quaternion(0.000, 0.000, 0.952, 0.305))
+            Point(-2.387, 3.771, 0.000),
+            Quaternion(0.000, 0.000, 0.462, 0.887))
         locations['loc_2'] = Pose(
-            Point(-8.872, 14.910, 0.000),
-            Quaternion(0.000, 0.000, -0.836, 0.549))
+            Point(-0.528, 6.085, 0.000),
+            Quaternion(0.000, 0.000, 0.378, 0.926))
+        locations['loc_3'] = Pose(
+            Point(-1.281, 9.811, 0.000),
+            Quaternion(0.000, 0.000, -0.654, 0.757))
         locations['loc_3'] = Pose(
             Point(-4.766, 12.545, 0.000),
             Quaternion(0.000, 0.000, 0.950, 0.314))
         locations['loc_4'] = Pose(
             Point(-1.727, 10.284, 0.000),
             Quaternion(0.000, 0.000, 0.986, 0.169))
-        locations['loc_5'] = Pose(
-            Point(0.947, 7.799, 0.000),
-            Quaternion(0.000, 0.000, 0.940, 0.340))
-        locations['loc_6'] = Pose(
-            Point(-1.116, 5.282, 0.000),
-            Quaternion(0.000, 0.000, 0.450, 0.893))
+        locations['loc_2'] = Pose(
+            Point(-8.872, 14.910, 0.000),
+            Quaternion(0.000, 0.000, -0.836, 0.549))
+        locations['loc_1'] = Pose(
+            Point(-13.079, 10.644, 0.000),
+            Quaternion(0.000, 0.000, 0.952, 0.305))
 
+
+
+ 
         route = ['loc_1', 'loc_2', 'loc_3', 'loc_4', 'loc_5', 'loc_6']
         # route = ['loc_1', 'loc_2']
 
